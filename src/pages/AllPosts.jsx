ac37a4d874ac7ab4ef960d6b78b671fdd3ff2d3a
deep-katkar <dnkatkar2000@gmail.com>
2024-03-05 19:15:58 +0530

    new changes

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -5,13 +5,13 @@ import { Container, PostCard } from "../components/index";
 function AllPosts() {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {}, []);
-
-  appwriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
-  });
+  useEffect(() => {
+    appwriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
 
   return (
     <div className="w-full py-8">
